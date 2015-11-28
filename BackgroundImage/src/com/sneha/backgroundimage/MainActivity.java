fdfa499197c2a26372bceee0d25e18c996e4f95d
gkrishnachaitanyagithub <krishnachaitanya939@gmail.com>
2015-11-28 14:02:33 +0530

    dbvashbv

---
+++ BackgroundImage/src/com/sneha/backgroundimage/MainActivity.java
@@ -0,0 +1,31 @@
+package com.sneha.backgroundimage;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle b) {
+		super.onCreate(b);
+		setContentView(R.layout.activity_main);
+
+		
+	}
+
+	
+
+	
+
+	
+
+}
