840d44c8522b8daf13485bc52c2a463f0d2768bf
gkrishnachaitanyagithub <krishnachaitanya939@gmail.com>
2015-12-08 11:41:11 +0530

    android app

---
+++ Registerapp/gen/com/chaitu/registerapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.chaitu.registerapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Registerapp/src/com/chaitu/registerapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.chaitu.registerapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+	}
+
+}
